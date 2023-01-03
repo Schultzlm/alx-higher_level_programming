70ff7b2f36db520bce51723e653df15abeb56e37
schultzlm <litokwas@programmer.net>
2023-01-03 15:24:12 -0800

    Task_3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 #Author - Schultz Litokwas
+
+#prints the ASCII alphabet, in lowercase.
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
