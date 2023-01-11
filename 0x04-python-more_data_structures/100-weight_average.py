65543736539f88aa72589956c1396730221db2ad
schultzlm <litokwas@programmer.net>
2023-01-11 04:05:12 -0800

    Weight_average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list and len(my_list):
-        num =0
+        num = 0
         denom = 0
         for tup in my_list:
             num += (tup[0] * tup[1])
