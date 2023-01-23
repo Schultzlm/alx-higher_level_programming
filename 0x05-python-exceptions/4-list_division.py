ee0cf48c5fcd4bbd3aee0981451a6a60591332fd
schultzlm <litokwas@programmer.net>
2023-01-23 09:28:01 -0800

    Tak_4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists by elements.
+
+    Args:
+        my_list_1 (list): The first list.
+        my_list_2 (list): Thesecond list.
+
+    Returns:
+        A new list of length list_length containing all the divisions.
+    """
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            div = my_list_1[1] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            new_list.append(div)
+        return (new_list)
