ff96948765d78c5f90102b3bcb87c232515bd511
schultzlm <litokwas@programmer.net>
2023-01-24 07:26:50 -0800

    Task_1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class square."""
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Intitialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
