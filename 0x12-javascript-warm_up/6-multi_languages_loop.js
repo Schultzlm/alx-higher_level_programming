45c49b98a79bacb895bb895b5ab68a0186fa8a84
schultzlm <litokwas@programmer.net>
2023-04-13 15:11:21 +0200

    js-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-// script to print strings using a loop
-
-let i;
-const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (i = 0; i < 3; i++) {
-  console.log(myString[i]);
+const arrayStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+for (i; i < arrayStrings.length; i++) {
+  console.log(arrayStrings[i]);
 }
